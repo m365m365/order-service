3c53c22500a40847e5eaf2dea749904269e86ccd
m365m365 <139079678+m365m365@users.noreply.github.com>
2026-06-09 14:48:44 +0800

    CorsConfig

---
+++ src/main/java/com/example/orderservice/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.example.orderservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration config =
+                new CorsConfiguration();
+
+        config.addAllowedOrigin("https://topfoodai.com");
+        config.addAllowedOrigin("https://www.topfoodai.com");
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
